01296d482e7d28e40db4badb4381c684c34d6008
FluffyPan <ilyavas.0@mail.ru>
2021-07-08 19:05:03 +0300

    18.6

---
+++ 18.6/config.py
@@ -0,0 +1,9 @@
+TOKEN = '1713213874:AAF5feHdzZtlz4qi-MrWvZTprx6iFNXYkYQ'
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+    'евро': 'EUR'
+}
---
+++ 18.6/extensions.py
@@ -0,0 +1,36 @@
+import requests
+import json
+from config import keys
+
+
+class ConversionException(Exception):
+    pass
+
+
+class CryptoConvertor:
+    @staticmethod
+    def get_price(quote: str, base: str, amount: str):
+        if quote == base:
+            raise ConversionException("две одинаковые валюты")
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConversionException(f"Не удалось обработать валюту {base}")
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConversionException(f"Не удалось обработать валюту {quote}")
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConversionException(f"Не удалось обработать количество{amount}")
+
+        r = requests.get(f"https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}"
+                         f"&tsyms={base_ticker}")
+
+        total_base = json.loads(r.content)[base_ticker]
+
+        return total_base * amount
